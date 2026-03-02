bf68f1bd435ae0a157d9c32a013b419a2c3e5103
LauraLivers <hello@lauralivers.com>
2026-03-02 13:14:08 +0100

    video to image file extended for folder handling

--- DiffCL/video2image.sh
+++ DiffCL/video2image.sh
@@ -1,3 +1,28 @@
-video = '$HOME/Downloads/20260301_c31_front.mkv'
+#!/usr/bin/env bash
 
-ffmpeg -i '$video' -vf select='between(t,1,5)+between(t,11,15)' -vsync 0 ${HOME}/Downloads/20260301_c31_front_%d.png ## extract specific window as frames
+# call file with './video2image.sh [input directory path] [output directory path]'
+# output folder will be automatically generated if nonexistent
+
+input_dir="$1"
+output_dir="$2"
+
+if [[ -z "$input_dir" || -z "$output_dir" ]]; then
+    echo "Usage: $0 <input_folder> <output_folder>"
+    exit 1
+fi
+
+mkdir -p "$output_dir"
+
+for video in "$input_dir"/*.{mkv}; do
+    [[ -e "$video" ]] || continue
+
+    filename=$(basename -- "$video")
+    name="${filename%.*}"
+
+    echo "Processing: $filename"
+
+    ffmpeg -i "$video" \
+        -vsync 0 \
+        -vf "select='not(mod(t,10))'" \
+        "$output_dir/${name}_%04d.png
+done
