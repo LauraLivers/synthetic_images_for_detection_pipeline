ff4aff3599d57d5c399f4409213b961cd450c036
Laura Livers <110529955+LauraLivers@users.noreply.github.com>
2026-06-08 12:38:59 +0200

    Delete Report directory

--- Report/HSLU_I_Thesis_Template/move_aux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Moves LaTeX auxiliary files to the latex_files/ subfolder after a build.
-cd "$(dirname "$0")"
-mkdir -p latex_files
-for ext in aux bbl blg fls fdb_latexmk log toc lot lof out acn acr alg glg glo gls idx ind ilg nav snm vrb bcf run.xml synctex.gz xmpi; do
-  for f in *.$ext; do
-    [ -f "$f" ] && mv "$f" latex_files/
-  done
-done
